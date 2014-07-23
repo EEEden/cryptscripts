f51883e2391d5a5a4d526eb474f908399a424a7c
EEEden <edendoyle@gmail.com>
2014-07-23 02:42:16 +0100

    added caesar cipher python script to compute the caesar cipher of some plaintext provided through command line arguments

---
+++ caesar.py
@@ -0,0 +1,20 @@
+#Caesar cipher
+from string import maketrans, translate, lowercase
+import sys
+
+if len(sys.argv) != 3:
+    print "Usage: caesar.py plaintext placestoshift"
+    exit(1)
+isLower = sys.argv[1][0].islower() #if the output is piped the it stays in the correct case
+plaintext = sys.argv[1].lower()
+shifter = int(sys.argv[2]) #cast to use as slice index
+
+alpha = lowercase
+shiftedAlpha = alpha[shifter:] + alpha[:shifter]
+table = maketrans(alpha, shiftedAlpha)
+
+ciphertext = plaintext.translate(table)
+if isLower:
+    print ciphertext
+else :
+    print ciphertext.upper()
